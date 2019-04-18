b6456990b5d95d5823a8392360dccd6305e86fb9
Paul Waldo <paul.w.waldo@gmail.com>
2019-04-18 16:32:08 -0400

    Swaps name and memo

--- ofx_swapper.py
+++ ofx_swapper.py
@@ -2,5 +2,29 @@ import xml.etree.ElementTree as ET
 
 tree = ET.parse('/Users/paul/Downloads/msmoneyExport.OFX')
 root = tree.getroot()
-for memo in root.iter('MEMO'):
-    print(memo.text)
+# print(ET.tostring(root))
+for transaction in root.iter('STMTTRN'):
+    name = transaction.find('NAME')
+    memo = transaction.find('MEMO')
+    if name == None:
+        print('Name missing')
+        continue
+    if memo == None:
+        continue
+    print(f'Found "{name.text}" and "{memo.text}"')
+    old_memo_text = memo.text
+    memo.text = name.text
+    name.text = old_memo_text
+
+print('***************************************************************************')
+for transaction in root.iter('STMTTRN'):
+    name = transaction.find('NAME')
+    memo = transaction.find('MEMO')
+    if name == None:
+        print('Name missing')
+        continue
+    if memo == None:
+        continue
+    print(f'Found "{name.text}" and "{memo.text}"')
+
+tree.write('modified.OFX')
