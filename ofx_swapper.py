1b9838c76ad4be560f73a58f5dc8bbc782b66eeb
Paul Waldo <paul.w.waldo@gmail.com>
2019-04-19 16:44:19 -0400

    Actually swaps name and memo

--- ofx_swapper.py
+++ ofx_swapper.py
@@ -5,33 +5,21 @@ class OFXSwapper:
     def __init__(self, inputFile):
         self._inputFile = inputFile
         self._tree = ET.parse(self._inputFile)
+        self._swap_names_and_memos()
 
-
-# tree = ET.parse('/Users/paul/Downloads/msmoneyExport.OFX')
-# root = tree.getroot()
-# # print(ET.tostring(root))
-# for transaction in root.iter('STMTTRN'):
-#     name = transaction.find('NAME')
-#     memo = transaction.find('MEMO')
-#     if name == None:
-#         print('Name missing')
-#         continue
-#     if memo == None:
-#         continue
-#     print(f'Found "{name.text}" and "{memo.text}"')
-#     old_memo_text = memo.text
-#     memo.text = name.text
-#     name.text = old_memo_text
-
-# print('***************************************************************************')
-# for transaction in root.iter('STMTTRN'):
-#     name = transaction.find('NAME')
-#     memo = transaction.find('MEMO')
-#     if name == None:
-#         print('Name missing')
-#         continue
-#     if memo == None:
-#         continue
-#     print(f'Found "{name.text}" and "{memo.text}"')
+    def _swap_names_and_memos(self):
+        root = self._tree.getroot()
+        for transaction in root.iter('STMTTRN'):
+            name = transaction.find('NAME')
+            memo = transaction.find('MEMO')
+            if name == None:
+                print('Name missing')
+                continue
+            if memo == None:
+                continue
+            print(f'Found "{name.text}" and "{memo.text}"')
+            old_memo_text = memo.text
+            memo.text = name.text
+            name.text = old_memo_text
 
 # tree.write('modified.OFX')
--- test_ofx_swapper.py
+++ test_ofx_swapper.py
@@ -8,12 +8,14 @@ class TestOFXSwapper(unittest.TestCase):
     def test_swap_name_and_memo(self):
         sut = OFXSwapper('test_data/test.ofx')
         root = sut._tree.getroot()
+
+        # Check the first transaction to see if it got swapped
         statements = root.iter('STMTTRN')
         self.assertIsNotNone(statements)
-        first_statement = statements
+        first_statement = next(statements)
         name = first_statement.find('NAME').text
-        memo = root.find('MEMO').text
         self.assertEqual(name, 'Merchant1')
+        memo = first_statement.find('MEMO').text
         self.assertEqual(memo, 'DEBIT CARD PURCHASE XXXXX1234')
 
 
